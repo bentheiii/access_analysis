1ef6967af93b60c0dc76a505761381f279d8ecb1
Daniel Kasman <kasmandanny@gmail.com>
2018-07-28 13:57:58 +0300

    Example files and results

--- tests/sandbox.py
+++ tests/sandbox.py
@@ -1,11 +1,9 @@
 from accessanalysis import *
-'''
 g = read(
     """
-    f0:(u0,r)
-    f1:(u0,w)->(u1,r)
-    f2:(u0,rw)->(u1,r)->(u2,r)->(u3,w)
-    f3:
+    f1:(u1,r)
+    f2:(u1,w)->(u2,r)
+    f3:(u1,w)
     """.splitlines()
 )
 ia = invalid_access(g)
@@ -15,7 +13,6 @@ if isinstance(up, str):
     print(up)
 else:
     print('\n'.join(f + ': ' + ' '.join(str(x) for x in r) for (f, r) in up.items()))
-'''
 #dot(g).render('out.gv', view=True, cleanup=True)
 
 g = read(
@@ -24,8 +21,6 @@ g = read(
     f2:(u1,w)->(u2,rw)->(u3,w)->(u4,r)
     f3:(u1,w)->(u2,r)->(u3,rw)->(u4,r)
     f4:(u1,w)->(u2,rw)->(u3,w)->(u4,r)
-    f5:(u1,w)->(u3,w)->(u2,rw)->(u4,r)
-    f6:(u6,r)
     """.splitlines()
 )
 print(blp(g))
